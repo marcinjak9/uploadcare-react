e680dae205b3fb5b32c7ec5f0bbedd8e40aec7f0
marcinjak9 <marcinjak9@gmail.com>
2019-04-28 23:08:09 +0200

    first prototype

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'uploadcare-react'
+import Image from 'uploadcare-react'
 
 export default class App extends Component {
   render () {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <Image 
+          src='https://ucarecdn.com/52da3bfc-7cd8-4861-8b05-126fef7a6994/'
+          preview="800x400"
+          // resize="300"
+          // crop={{ dimensions: '440x380', position: 'center' }}
+          // scaleCrop={{ dimensions: '200x200' }}
+          // stretch="off"
+          // setfill="333333"
+          // quality="lightest"
+          // progressive="yes"
+          // format="png"
+          // enhance={0}
+          // sharp={20}
+          // blur={{ strength: 100, amount: -200 }}
+          colors={{
+            // brightness: 0,
+            // exposure: -50,
+            // gamma: 500,
+            // contrast: 100,
+            // saturation: 100,
+            // vibrance: 300,
+            // warmth: 100,
+          }}
+          // filter={{
+          //   name: 'pamaya',
+          //   amount: 50
+          // }}
+          // grayscale
+          // invert
+          // autorotate="no"
+          // rotate={90}
+          // flip
+          // mirror
+        />
       </div>
     )
   }
---
+++ src/utils/index.tsx
@@ -0,0 +1,96 @@
+import { Props } from "..";
+
+export const buildUrl = (props: Props): string => {
+  const {
+    preview, src, resize, crop, scaleCrop, stretch,
+    setfill, format, quality, progressive, enhance,
+    sharp, blur, colors, filter, grayscale, invert,
+    autorotate, rotate, flip, mirror,
+  } = props;
+  let url = src;
+  // TODO: detect if final / is missing
+  if (typeof preview === 'string') {
+    url += `-/preview/${preview}/`
+  } else if (typeof preview === 'boolean') {
+    url += '-/preview/'
+  }
+  if (resize) {
+    url += `-/resize/${resize}/`
+  }
+  if (crop && crop.dimensions) {
+    url += `-/crop/${crop.dimensions}/${crop.position ? `${crop.position}/` : ''}`
+  }
+  if (scaleCrop && scaleCrop.dimensions) {
+    url += `-/scale_crop/${scaleCrop.dimensions}/${scaleCrop.position ? `${scaleCrop.position}/` : ''}`
+  }
+  if (stretch) {
+    url += `-/stretch/${stretch}/`
+  }
+  if (setfill) {
+    url += `-/setfill/${setfill}/`
+  }
+  if (format) {
+    url += `-/format/${format}/`
+  }
+  if (quality) {
+    url += `-/quality/${quality}/`
+  }
+  if (progressive) {
+    url += `-/progressive/${progressive}/`
+  }
+  if (enhance) {
+    url += `-/enhance/${enhance}/`
+  }
+  if (sharp) {
+    url += `-/sharp/${sharp}/`
+  }
+  if (blur && blur.strength) {
+    url += `-/blur/${blur.strength}/${blur.amount ? `${blur.amount}/` : ''}`
+  }
+  if (colors) {
+    if (colors.brightness) {
+      url += `-/brightness/${colors.brightness}/`
+    }
+    if (colors.exposure) {
+      url += `-/exposure/${colors.exposure}/`
+    }
+    if (colors.gamma) {
+      url += `-/gamma/${colors.gamma}/`
+    }
+    if (colors.contrast) {
+      url += `-/contrast/${colors.contrast}/`
+    }
+    if (colors.saturation) {
+      url += `-/saturation/${colors.saturation}/`
+    }
+    if (colors.vibrance) {
+      url += `-/vibrance/${colors.vibrance}/`
+    }
+    if (colors.warmth) {
+      url += `-/warmth/${colors.warmth}/`
+    }
+  }
+
+  if (filter && filter.name) {
+    url += `-/filter/${filter.name}/${filter.amount ? `${filter.amount}/` : ''}`
+  }
+  if (grayscale) {
+    url += '-/grayscale/'
+  }
+  if (invert) {
+    url += '-/invert/'
+  }
+  if (autorotate) {
+    url += `-/autorotate/${autorotate}/`
+  }
+  if (rotate) {
+    url += `-/rotate/${rotate}/`
+  }
+  if (flip) {
+    url += `-/flip/`
+  }
+  if (mirror) {
+    url += `-/mirror/`
+  }
+  return url;
+}
